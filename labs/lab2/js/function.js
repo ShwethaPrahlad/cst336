3e1fe25de534c001421b4fb22dbe14a6cc349f1c
Shwetha Prahlad <sprahlad@csumb.edu>
2019-02-10 06:42:26 +0000

    adding labs2

--- labs/lab2/js/function.js
+++ labs/lab2/js/function.js
@@ -1,1 +1,68 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+            var randomNumber = Math.floor(Math.random() * 99) + 1;
+            //console.log (randomNumber);
+            var guesses = document.querySelector("#guesses");
+            var lastResult = document.querySelector('#lastResult');
+            var lowOrHi = document.querySelector('#lowOrHi');
+            var guessSubmit = document.querySelector(".guessSubmit");
+            var guessField = document.querySelector(".guessField");
+            var guessCount = 1;
+            var resetButton = document.querySelector("#reset");
+            resetButton.style.display = 'none';
+            guessField.focus();
+            //var resetButton;
+            
+            function checkGuess(){
+                var userGuess = Number(guessField.value);
+                if (guessCount === 1) {
+                guesses.innerHTML = 'Previous guesses: ';
+                }
+             guesses.innerHTML += userGuess + ' ';
+             if (userGuess === randomNumber){
+              lastResult.innerHTML = 'Congratulations, you got it right!';
+              lastResult.style.backgroundColor= 'green';
+              lowOrHi.innerHTML = '';
+              setGameOver();
+             }else if (guessCount === 7){
+              lastResult.innerHTML = 'Sorry, you lost!';
+              setGameOver();
+             }else {
+              lastResult.innerHTML = 'Wrong!';
+              lastResult.style.backgroundColor= 'red';
+              if (userGuess < randomNumber){
+              lowOrHi.innerHTML = 'Your last guess was too low!';
+              }
+              if (userGuess > randomNumber){
+              lowOrHi.innerHTML = 'Your last guess was too high!';
+              }
+             }
+             guessCount++;
+             guessField.value = '';
+             guessField.focus();
+            }
+            
+            guessSubmit.addEventListener ('click', checkGuess);
+            
+            function setGameOver(){
+                guessField.disabled = true;
+                guessSubmit.disabled = true;
+                resetButton.style.display = 'inline';
+                resetButton.addEventListener('click', resetGame);
+            }
+            function resetGame(){
+                guessCount = 1;
+                var resetParas = document.querySelectorAll('.resultParas p');
+                for (var i=0; i < resetParas.length ; i++){
+                    resetParas[i].textContent = '';
+                }
+                resetButton.style.display = 'none';
+                guessField.disabled = false;
+                guessSubmit.disabled = false;
+                guessField.value = '';
+                guessField.focus();
+                lastResult.style.backgroundColor= 'white';
+                randomNumber = Math.floor(Math.random() * 99) + 1;
+                //console.log (randomNumber);
+            }
+            $("#guessHeader").append("!");
+            $("#submitBtn").css("background-color", "cyan");
+
