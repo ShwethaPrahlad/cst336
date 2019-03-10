a01f8eaa2dac6234e5613f1136c6d6399bf2db5d
Shwetha Prahlad <sprahlad@csumb.edu>
2019-03-10 08:23:59 +0000

    adding some styling updates

--- labs/lab5/api/validateUsernameAPI.php
+++ labs/lab5/api/validateUsernameAPI.php
@@ -9,12 +9,12 @@ $pwd = $_GET['password'];
 //echo $username."<br>";
 //echo $password."<br>";
 
-$pword = array();
-if(stripos ($username, $password) !== false) {
-    $pword['validPwd'] = false;
+$vPwd = array();
+if(stripos ($username, $pwd) !== false) {
+    $vPwd['validPwd'] = false;
 }
 else {
-    $pword['validPwd'] = true;
+    $vPwd['validPwd'] = true;
 }
-echo json_encode($pword);
+echo json_encode($vPwd);
 ?>
