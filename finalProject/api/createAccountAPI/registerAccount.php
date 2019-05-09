1e03854315683930cda7ea0a31fb0ad8ff8e1cd9
Shwetha Prahlad <sprahlad@csumb.edu>
2019-05-09 22:21:00 +0000

    sha1 encryption

--- finalProject/api/createAccountAPI/registerAccount.php
+++ finalProject/api/createAccountAPI/registerAccount.php
@@ -7,7 +7,7 @@
     $np[':email'] = $_GET['email'];//$_GET['name'] matches the ajax
     $np[':username']=$_GET['username'];
     $np[':fullname'] = $_GET['fullname']; 
-    $np[':password'] = $_GET['password']; 
+    $np[':password'] = sha1($_GET['password']); 
     
     //$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
         if (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)){
